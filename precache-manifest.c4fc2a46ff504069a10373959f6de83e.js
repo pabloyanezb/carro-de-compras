6b66ad7805f7077943b0af17c750f2542177327c
pabloyanezb <pabloyanezbarriga@gmail.com>
2020-10-28 18:50:02 -0300

    Updates

---
+++ precache-manifest.c4fc2a46ff504069a10373959f6de83e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2606176018fef4a1c374",
+    "url": "/carro-de-compras/static/css/main.0537a30e.chunk.css"
+  },
+  {
+    "revision": "2606176018fef4a1c374",
+    "url": "/carro-de-compras/static/js/main.2ee0e481.chunk.js"
+  },
+  {
+    "revision": "7c0cf45a843fefdd4856",
+    "url": "/carro-de-compras/static/js/runtime~main.964fe502.js"
+  },
+  {
+    "revision": "20e4adfe0d620a81f7c2",
+    "url": "/carro-de-compras/static/css/2.a0cc028d.chunk.css"
+  },
+  {
+    "revision": "20e4adfe0d620a81f7c2",
+    "url": "/carro-de-compras/static/js/2.94515e12.chunk.js"
+  },
+  {
+    "revision": "2d18c10f2ee9e6339707490ccac2d130",
+    "url": "/carro-de-compras/index.html"
+  }
+];
